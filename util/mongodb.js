d812937ed77b3ae3efc5338be1b4b04e3cb0aa15
Joey Schrader <jschrader@overstock.com>
2021-03-05 09:51:45 -0800

    Add Linting configuration

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,26 +1,26 @@
 import { MongoClient } from 'mongodb'
 
 const {
-  MONGODB_URI,
-  MONGODB_DB,
-  MONGODB_URI_PRODUCTION,
-  MONGODB_PRODUCTION,
-  NODE_ENV
+	MONGODB_URI,
+	MONGODB_DB,
+	MONGODB_URI_PRODUCTION,
+	MONGODB_PRODUCTION,
+	NODE_ENV,
 } = process.env
 
 const DATABASE_URI = NODE_ENV === 'production' ? MONGODB_URI_PRODUCTION : MONGODB_URI
 const DATABASE = NODE_ENV === 'production' ? MONGODB_PRODUCTION : MONGODB_DB
 
 if (!DATABASE_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
+	throw new Error(
+		'Please define the MONGODB_URI environment variable inside .env.local',
+	)
 }
 
 if (!DATABASE) {
-  throw new Error(
-    'Please define the MONGODB_DB environment variable inside .env.local'
-  )
+	throw new Error(
+		'Please define the MONGODB_DB environment variable inside .env.local',
+	)
 }
 
 /**
@@ -31,27 +31,27 @@ if (!DATABASE) {
 let cached = global.mongo
 
 if (!cached) {
-  cached = global.mongo = { conn: null, promise: null }
+	// eslint-disable-next-line no-multi-assign
+	cached = global.mongo = { conn: null, promise: null }
 }
 
+// eslint-disable-next-line import/prefer-default-export
 export async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn
-  }
+	if (cached.conn) {
+		return cached.conn
+	}
 
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+	if (!cached.promise) {
+		const opts = {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
 
-    cached.promise = MongoClient.connect(DATABASE_URI, opts).then((client) => {
-      return {
-        client,
-        db: client.db(DATABASE),
-      }
-    })
-  }
-  cached.conn = await cached.promise
-  return cached.conn
+		cached.promise = MongoClient.connect(DATABASE_URI, opts).then((client) => ({
+			client,
+			db: client.db(DATABASE),
+		}))
+	}
+	cached.conn = await cached.promise
+	return cached.conn
 }
