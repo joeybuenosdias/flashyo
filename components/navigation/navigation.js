aa97278c4cebb8803637cce2bf34cd7b4e128de1
Joey Schrader <jschrader@overstock.com>
2021-03-09 05:52:32 -0800

    Complete basic navigation links

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
+/** components */
+import Link from 'next/link'
+
+const links = [
+	{
+		path: '/',
+		name: 'Home',
+	},
+	{
+		path: '/about',
+		name: 'About',
+	},
+	{
+		path: '/decks',
+		name: 'Decks',
+	},
+]
+
 export default function Navigation() {
 	return (
-		<div>
-			Navigation
-		</div>
+		<nav>
+			<ul style={{ padding: '0px' }}>
+				{links.map((link) => (
+					<Link href={link.path}>
+						{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+						<a style={{ marginRight: '16px' }}>{link.name}</a>
+					</Link>
+				))}
+			</ul>
+		</nav>
 	)
 }
